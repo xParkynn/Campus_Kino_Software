d3e903db4ef6103602bbc8b7550cbee841024ce2
Maxim Schmidt <s-msch37@haw-landshut.de>
2024-11-27 15:10:32 +0100

    use shared variables now

--- Program/Program.py
+++ Program/Program.py
@@ -1,18 +1,18 @@
 import reflex as rx
-from backend_pandas import TicketManager
+from Program.shared import TiMg
 from assets.pages.ModifierPage import modify_page
 from assets.pages.AddPage import add_movie
 from assets.pages.HomePage import sidebar
-from assets.pages.Ticket_Home import Ticket_Home
-import sys
+from assets.pages.Ticket_Home import Ticket_Home, TicketState
+from Program.shared import app
 
 
-TiMg = TicketManager()
-app = rx.App()
-for i in TiMg.db.index:
-    if i == "Total":
-        break
-    app.add_page(lambda i=i: modify_page(i, TiMg.db.loc[i]["Movie"]), title=f"Ticket-Management ({TiMg.db.loc[i]["Movie"]})", route=f"/TiMg_{TiMg.db.loc[i]["Movie"]}")
+
+
+
+app.add_page(modify_page(), title=f"Ticket-Management", route=f"/TiMg")
 app.add_page(add_movie, title="Add Ticket", route="/add_ticket")
 app.add_page(sidebar, title="Home", route="/")
-app.add_page(Ticket_Home, title="Ticket-Management", route="/TicketManagement")
+app.add_page(Ticket_Home, title="Ticket-Management", route="/TicketManagement", on_load=TicketState.load())
+
+
