0e4e8604d75263388575e1b2ba6c70665724ce0e
Maxim Schmidt <s-msch37@haw-landshut.de>
2024-11-27 15:51:43 +0100

    added Live_Pages

--- Program/Program.py
+++ Program/Program.py
@@ -4,6 +4,8 @@ from assets.pages.ModifierPage import modify_page
 from assets.pages.AddPage import add_movie
 from assets.pages.HomePage import sidebar
 from assets.pages.Ticket_Home import Ticket_Home, TicketState
+from assets.pages.LivePage import live_page
+from assets.pages.LiveMode import live_mode
 from Program.shared import app
 
 
@@ -11,8 +13,10 @@ from Program.shared import app
 
 
 app.add_page(modify_page(), title=f"Ticket-Management", route=f"/TiMg")
-app.add_page(add_movie, title="Add Ticket", route="/add_ticket")
+app.add_page(add_movie, title="Add Ticket", route="/add_movie")
 app.add_page(sidebar, title="Home", route="/")
 app.add_page(Ticket_Home, title="Ticket-Management", route="/TicketManagement", on_load=TicketState.load())
+app.add_page(live_page, title="Live-Management", route="/live")
+app.add_page(live_mode, title="Live Mode", route="/live_mode")
 
 
